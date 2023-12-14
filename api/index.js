8d00e7ca685435b178151916793b68b51e215114
Tyron_Wei <u20671572@tuks.co.za>
2023-12-14 23:01:10 +0200

    updated form with json

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
 app.post('/register', (req,res)=>{
-    res.json('test ok');
+    const {username, password} = req.body;
+    res.json({requestData:{username,password}});
 });
+
 app.listen(4000);
