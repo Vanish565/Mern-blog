97dc05323db63bc6d30954aa86c689f9be899c9a
Tyron_Wei <u20671572@tuks.co.za>
2023-12-14 22:23:13 +0200

    Updated Layout and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Header from './Header';
+import Post from './Post';
+import Layout from './Layout';
 
 function App() {
   return (
-    <div>test</div>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<Post/>}/>
+        <Route path='/login' element={<div>login</div>}/>
+        <Route path='register' element={<div>register</div>}/>
+      </Route>
+    </Routes>
+    
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,18 @@
+export default function Post()
+{
+    return (
+        <div className='post'>
+        <div className='image'>
+          <img src='https://www.technewsworld.com/wp-content/uploads/sites/3/2023/11/computer-chip.jpg' alt=''></img>
+        </div>
+        <div className='texts'>
+          <h2>Chip Wars of 2024: Will a Cell Phone Take the Laptop Crown?</h2>
+          <p className='info'>
+            <a className='author'>Rob Enderle</a>
+            <time>November 6, 2023 5:00 AM PT</time>
+          </p>
+          <p className='summary'>As industry leaders in chip manufacturing vie for supremacy, there’s mounting speculation that the capabilities of smartphones might soon rival the traditional dominance of laptops.</p>
+        </div>
+      </div>
+    );
+}
