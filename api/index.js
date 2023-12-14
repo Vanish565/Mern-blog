b3ae06d667231d27576b1b3a07aa5407b24db8b0
Tyron_Wei <u20671572@tuks.co.za>
2023-12-14 23:13:52 +0200

    connected to dB

--- api/index.js
+++ api/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req,res)=>{
+mongoosegoose.connect('mongodb+srv://tyronwei:Tyron2009@mern-blog.dbrumwd.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async(req,res)=>{
     const {username, password} = req.body;
-    res.json({requestData:{username,password}});
+    const userDoc = await User.create({username,password});
+    res.json(userDoc);
 });
 
 app.listen(4000);
-//mongodb+srv://tyronwei:Tyron2009@mern-blog.dbrumwd.mongodb.net/?retryWrites=true&w=majority
