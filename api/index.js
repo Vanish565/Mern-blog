9804af5d5871af1464ce741356064c0522588f68
Tyron_Wei <u20671572@tuks.co.za>
2023-12-15 14:17:30 +0200

    encryption package added

--- api/index.js
+++ api/index.js
@@ -2,17 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
+
+salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
 
+//connecting to database
 mongoose.connect('mongodb+srv://tyronwei:Tyron2009@mern-blog.dbrumwd.mongodb.net/?retryWrites=true&w=majority');
 
 app.post('/register', async(req,res)=>{
     const {username, password} = req.body;
     try{
-        const userDoc = await User.create({username,password});
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password,salt)});
         res.json(userDoc);
     }
     catch(e){
