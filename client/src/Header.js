885e75252ac7738fd21c620d3bb658a2a1281b47
Tyron_Wei <u20671572@tuks.co.za>
2023-12-15 22:20:18 +0200

    fixed refreshing with login

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 export default function Header()
 {
-  const [username,setUsername] = useState(null);
+  // grabbing the user info from the UserContext.js
+  const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect(() =>{
     fetch('http://localhost:4000/profile', {
       credentials: 'include',
     }).then(response=> {
       response.json().then(userInfo =>{
-        setUsername(userInfo.username);
+        setUserInfo(userInfo);
       });
     });
   },[]);
@@ -20,9 +22,9 @@ export default function Header()
       credentials: 'include',
       method: 'POST',
     });
-    setUsername(null);
+    setUserInfo(null);
   }
-
+    const username = userInfo?.username;
     return(
         <header>
         <Link to='/' className='logo'>MY BLOG</Link>
