e438847a07b7cd7d7d35f11ea378f2f7e7d1fa0d
Tyron_Wei <u20671572@tuks.co.za>
2023-12-27 21:09:28 +0200

    css styling for PostPage

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,3 +1,4 @@
+import { formatISO9075 } from "date-fns";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -16,11 +17,13 @@ export default function PostPage(){
 
     return(
         <div className="post-page">
+            <h1>{postInfo.title}</h1>
+            <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+            <div className="author"> by {postInfo.author.username}</div>
             <div className="image">
              <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
             </div>
-            <h1>{postInfo.title}</h1>
-            <div dangerouslySetInnerHTML={{__html:postInfo.content}}/>
+            <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}}/>
         </div>
     );
 }
